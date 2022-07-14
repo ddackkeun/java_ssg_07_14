1066243a438b3f3874d0bc572649012b0c977e0a
seungkeun <tmdrms0705@gmail.com>
2022-07-14 15:51:24 +0900

    [추가] JUnit 추가 & 테스트 입력 - JUnit 의존성 추가 및 테스트 - 표준 입출력 리다이렉션 테스트

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,11 +1,19 @@
 package com.ll.exam;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    Scanner sc;
     int sequence;
+    List<WiseSaying> wiseSayingList;
+
     public void run() {
-        Scanner sc = new Scanner(System.in);
+        sc = new Scanner(System.in);
+        wiseSayingList = new ArrayList<>();
+
+        sequence = 0;
 
         System.out.println("== 명언 SSG ==");
 
@@ -17,11 +25,12 @@ public class App {
             switch(cmd){
                 case "등록":
                     System.out.print("명언 : ");
-                    String wiseSaying = sc.nextLine().trim();
+                    String content = sc.nextLine().trim();
                     System.out.print("작가 : ");
                     String author = sc.nextLine().trim();
-                    sequence++;
-                    System.out.println( sequence + "번 명언이 등록되었습니다.");
+                    int id = ++sequence;
+                    WiseSaying wiseSaying = new WiseSaying(id, content, author);
+                    System.out.println(wiseSaying.getId() +"번 명언이 등록되었습니다.");
                     break;
 
                 case "종료":
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,8 +1,52 @@
 package com.ll.exam;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
+    @Test
+    void junit_테스트(){
+        int a = 10 + 20;
+        assertEquals(30, a);
+    }
 
+    @Test
+    void 문자열스캐너_테스트() {
+        String input = """
+                등록
+                명언1
+                작가1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("등록", cmd);
+        assertEquals("명언1", content);
+        assertEquals("작가1", author);
+    }
+
+    @Test
+    void 표준출력_테스트() throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+        String result = output.toString().trim();
+
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", result);
+    }
 }
